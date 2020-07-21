261f504a0e65886e7411f77a5427bb7be472e42c
Kejane <haijuan_ke@yeah.net>
2020-07-21 20:15:40 +0800

    update format print

--- upload.py
+++ upload.py
@@ -74,10 +74,19 @@ if __name__ == "__main__":
     upload_task = YiQingTong(args)
     scheduler = BlockingScheduler()
     # 晨检
-    scheduler.add_job(upload_task.upload, 'cron', hour=random.randint(6,11), minute=random.randint(1,59))
+    morning_hour=random.randint(6,11)
+    morning_minute=random.randint(1,59)
+    print("晨检将会在每天{:0>2d}:{:0>2d}填报".format(morning_hour,morning_minute))
+    scheduler.add_job(upload_task.upload, 'cron', hour=morning_hour, minute=morning_minute)
     # 午检
+    noon_hour=random.randint(12,18)
+    noon_minute=random.randint(1,59)
+    print("午检将会在每天{:0>2d}:{:0>2d}填报".format(noon_hour,noon_minute))
     scheduler.add_job(upload_task.upload, 'cron', hour=random.randint(12,18), minute=random.randint(1,59))
     # 晚检
+    night_hour=random.randint(19,22)
+    night_minute=random.randint(1,59)
+    print("晚检将会在每天{:0>2d}:{:0>2d}填报".format(night_hour,night_minute))
     scheduler.add_job(upload_task.upload, 'cron', hour=random.randint(19,22), minute=random.randint(1,59))
     scheduler.start()
 
